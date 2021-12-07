203fab8159d4d6e033535f73dabb9d63e5d1ae0c
Scarlet <2025294985@qq.com>
2021-12-07 15:38:37 +0800

    1.3.0

--- main.py
+++ main.py
@@ -5,23 +5,33 @@ import time
 import MyNode
 import MyScheduler
 import MyEventListManagement
+import MyEventListManagement
 
-N = 3
-times = 60
-FrameList = MyEventListManagement.EventListManagement(N, times)
-# FrameList.MessageDefine(0, 0, "Frame 0,0")
-# FrameList.MessageDefine(1, 0, "Frame 1,0")
-# FrameList.MessageDefine(2, 1, "Frame 2,1")
-# FrameList.DefineEvents(0,0,5,5,"节点0 事件测试")
-# FrameList.DefineEvents(1,1,4,5,"节点1 事件测试")
+N = 5
+times = 100
+MyNode.Node.p=0.1
+elm = MyEventListManagement.EventListManagement(N, times)
+elm.DefinePoissonEvents(endslot=times, lamb=2)
+clk = MyScheduler.Scheduler(N, times, elm.FinishDefine())
+clk.StartSimulation()
+print(clk.CalcEffciency())
 
-# FrameList.DefineEvent(2,1,"节点2 事件测试")
-# FrameList.DefineEvent(1,0,"节点2 事件测试")
-# FrameList.DefineEvent(0,0,"节点2 事件测试")
-# FrameList.DefineParetoEvents(endslot=20)
-FrameList.DefinePoissonEvents(endslot=20, lamb=2)
+# N = 3
+# times = 60
+# FrameList = MyEventListManagement.EventListManagement(N, times)
+# # FrameList.MessageDefine(0, 0, "Frame 0,0")
+# # FrameList.MessageDefine(1, 0, "Frame 1,0")
+# # FrameList.MessageDefine(2, 1, "Frame 2,1")
+# # FrameList.DefineEvents(0,0,5,5,"节点0 事件测试")
+# # FrameList.DefineEvents(1,1,4,5,"节点1 事件测试")
 
-# FrameList.DefineEventB(0,0,1,5,"节点0 B事件测试多事件")
+# # FrameList.DefineEvent(2,1,"节点2 事件测试")
+# # FrameList.DefineEvent(1,0,"节点2 事件测试")
+# # FrameList.DefineEvent(0,0,"节点2 事件测试")
+# # FrameList.DefineParetoEvents(endslot=20)
+# FrameList.DefinePoissonEvents(endslot=20, lamb=2)
 
-Clock = MyScheduler.Scheduler(N, times, FrameList.FinishDefine())
-Clock.StartSimulation()
+# # FrameList.DefineEventB(0,0,1,5,"节点0 B事件测试多事件")
+
+# Clock = MyScheduler.Scheduler(N, times, FrameList.FinishDefine())
+# Clock.StartSimulation()
