8c4490d39874ce1cebfdabc978d4fd0768bedd58
Scarlet <2025294985@qq.com>
2021-12-10 13:46:50 +0800

    3.0.0

--- main.py
+++ main.py
@@ -10,62 +10,36 @@ import CSMACDEventListManagement
 import CSMACDScheduler
 
 def TestSlottedAloha():
-    N = 5
+    N = 4
     times = 100
     SANode.Node.p=0.25
     elm = SAEventListManagement.EventListManagement(N, times)
     elm.DefinePoissonEvents(endslot=times, lamb=2)
     clk = SAScheduler.Scheduler(N, times, elm.FinishDefine())
     clk.StartSimulation()
-    # clk.print()
+    clk.print()
     print("效率%.2f%%"%(clk.CalcEffciency()*100))
 
-# TestSlottedAloha()
 
-CSMACDNode.Node.IFG=5 #定义帧间缝隙
-# N1=CSMACDNode.Node()
-# N2=CSMACDNode.Node()
-# N1.SendFrame()
-# print(N2.Crowd)
-N = 3
-times = 60
+def TestCSMACD():
 
-FrameList = CSMACDEventListManagement.EventListManagement(N, times)
+    CSMACDNode.Node.IFG=5 #定义帧间缝隙
+    N = 3
+    times = 60
 
-FrameList.DefineEvent(2,1,"节点2 事件测试",10)
-FrameList.DefineEvent(1,1,"节点1 事件测试",10)
-FrameList.DefineEvent(0,1,"节点0 事件测试",10)
+    FrameList = CSMACDEventListManagement.EventListManagement(N, times)
+
+    FrameList.DefineEvent(2,1,"节点2 事件测试",10)
+    FrameList.DefineEvent(1,1,"节点1 事件测试",10)
+    FrameList.DefineEvent(0,1,"节点0 事件测试",10)
 
 
 
-Clock = CSMACDScheduler.Scheduler(N, times, FrameList.FinishDefine())
-Clock.StartSimulation()
-Clock.print()
+    Clock = CSMACDScheduler.Scheduler(N, times, FrameList.FinishDefine())
+    Clock.StartSimulation()
+    Clock.print()
 
 
+TestSlottedAloha()
+# TestCSMACD()
 
-
-
-
-
-
-
-# N = 3
-# times = 60
-# FrameList = MyEventListManagement.EventListManagement(N, times)
-# # FrameList.MessageDefine(0, 0, "Frame 0,0")
-# # FrameList.MessageDefine(1, 0, "Frame 1,0")
-# # FrameList.MessageDefine(2, 1, "Frame 2,1")
-# # FrameList.DefineEvents(0,0,5,5,"节点0 事件测试")
-# # FrameList.DefineEvents(1,1,4,5,"节点1 事件测试")
-
-# # FrameList.DefineEvent(2,1,"节点2 事件测试")
-# # FrameList.DefineEvent(1,0,"节点2 事件测试")
-# # FrameList.DefineEvent(0,0,"节点2 事件测试")
-# # FrameList.DefineParetoEvents(endslot=20)
-# FrameList.DefinePoissonEvents(endslot=20, lamb=2)
-
-# # FrameList.DefineEventB(0,0,1,5,"节点0 B事件测试多事件")
-
-# Clock = MyScheduler.Scheduler(N, times, FrameList.FinishDefine())
-# Clock.StartSimulation()
